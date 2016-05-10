28a078fda7b4fecfe9f541fcfe3dfb8829ee6e8d
Daniel <daniel.bujold@gmail.com>
2016-05-10 13:21:07 -0300

    Model for User/Controller/View

---
+++ RmR/DAL/ResumeContext.cs
@@ -0,0 +1,17 @@
+﻿using RmR.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RmR.DAL
+{
+    class ResumeContext : DbContext
+    {
+        public ResumeContext() : base("DefaultConnection")
+        { }
+        public DbSet<User> Users { get; set; }
+    }
+}
--- RmR/Models/User.cs
+++ RmR/Models/User.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,6 +9,28 @@ namespace RmR.Models
 {
     public class User
     {
+        public int ID { get; set; }
+        [Required]
+        [StringLength(50)]
+        [Display(Name = "Full Name")]
+        public string FirstName { get; set; }
+        [Required]
+        [StringLength(60)]
+        [Display(Name = "Last Name")]
+        public string LastName { get; set; }
+        [Required]
+        [StringLength(80)]
+        public string Email { get; set; }
 
+        [Display(Name = "Full Name")]
+        public string FullName
+        {
+            get
+            {
+                return LastName + ", " + FirstName;
+            }
+        }
+        public virtual ICollection<User> Users { get; set; }
+        
     }
 }
