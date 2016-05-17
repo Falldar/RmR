6da682bcc295290de0a9b31baf2f209f8dce2c5b
Daniel <daniel.bujold@gmail.com>
2016-05-16 22:17:22 -0300

    added models for ClientResume and ExpertResume

---
+++ RmR/Controllers/ClientResumeController.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using RmR.DAL;
+using RmR.Models;
+
+namespace RmR.Controllers
+{
+    public class ClientResumeController : Controller
+    {
+        private ResumeContext db = new ResumeContext();
+
+        // GET: ClientResume
+        public ActionResult Index()
+        {
+            return View(db.Resumes.ToList());
+        }
+
+        // GET: ClientResume/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Resume resume = db.Resumes.Find(id);
+            if (resume == null)
+            {
+                return HttpNotFound();
+            }
+            return View(resume);
+        }
+
+        // GET: ClientResume/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: ClientResume/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ResumeID,ResumeName,CreatedOn,CompletedOn,Status,Description")] Resume resume)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Resumes.Add(resume);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(resume);
+        }
+
+        // GET: ClientResume/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Resume resume = db.Resumes.Find(id);
+            if (resume == null)
+            {
+                return HttpNotFound();
+            }
+            return View(resume);
+        }
+
+        // POST: ClientResume/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ResumeID,ResumeName,CreatedOn,CompletedOn,Status,Description")] Resume resume)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(resume).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(resume);
+        }
+
+        // GET: ClientResume/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Resume resume = db.Resumes.Find(id);
+            if (resume == null)
+            {
+                return HttpNotFound();
+            }
+            return View(resume);
+        }
+
+        // POST: ClientResume/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Resume resume = db.Resumes.Find(id);
+            db.Resumes.Remove(resume);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
