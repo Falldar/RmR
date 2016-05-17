1398d915acd922671ef8ac3c35663529e15b7503
Denis Ouellette <Denis Ouellette@gmail.com>
2016-05-16 22:14:16 -0300

    push before db fix, again

--- RmR/Controllers/ClientController.cs
+++ RmR/Controllers/ClientController.cs
@@ -59,63 +59,6 @@ namespace RmR.Controllers
             return View(client);
         }
 
-        // GET: Client/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Client client = db.Clients.Find(id);
-            if (client == null)
-            {
-                return HttpNotFound();
-            }
-            return View(client);
-        }
-
-        // POST: Client/Edit/5
-        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
-        // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,FirstName,LastName,Email")] Client client)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(client).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(client);
-        }
-
-        // GET: Client/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Client client = db.Clients.Find(id);
-            if (client == null)
-            {
-                return HttpNotFound();
-            }
-            return View(client);
-        }
-
-        // POST: Client/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Client client = db.Clients.Find(id);
-            db.Clients.Remove(client);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
-
         protected override void Dispose(bool disposing)
         {
             if (disposing)
--- RmR/Controllers/ExpertController.cs
+++ RmR/Controllers/ExpertController.cs
@@ -60,61 +60,61 @@ namespace RmR.Controllers
         }
 
         // GET: Expert/Edit/5
-        public ActionResult Edit(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Expert expert = db.Experts.Find(id);
-            if (expert == null)
-            {
-                return HttpNotFound();
-            }
-            return View(expert);
-        }
+        //public ActionResult Edit(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Expert expert = db.Experts.Find(id);
+        //    if (expert == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(expert);
+        //}
 
         // POST: Expert/Edit/5
         // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
         // more details see http://go.microsoft.com/fwlink/?LinkId=317598.
-        [HttpPost]
-        [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "ID,FirstName,LastName,Email")] Expert expert)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(expert).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            return View(expert);
-        }
+        //[HttpPost]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult Edit([Bind(Include = "ID,FirstName,LastName,Email")] Expert expert)
+        //{
+        //    if (ModelState.IsValid)
+        //    {
+        //        db.Entry(expert).State = EntityState.Modified;
+        //        db.SaveChanges();
+        //        return RedirectToAction("Index");
+        //    }
+        //    return View(expert);
+        //}
 
         // GET: Expert/Delete/5
-        public ActionResult Delete(int? id)
-        {
-            if (id == null)
-            {
-                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
-            }
-            Expert expert = db.Experts.Find(id);
-            if (expert == null)
-            {
-                return HttpNotFound();
-            }
-            return View(expert);
-        }
+        //public ActionResult Delete(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+        //    }
+        //    Expert expert = db.Experts.Find(id);
+        //    if (expert == null)
+        //    {
+        //        return HttpNotFound();
+        //    }
+        //    return View(expert);
+        //}
 
         // POST: Expert/Delete/5
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public ActionResult DeleteConfirmed(int id)
-        {
-            Expert expert = db.Experts.Find(id);
-            db.Experts.Remove(expert);
-            db.SaveChanges();
-            return RedirectToAction("Index");
-        }
+        //[HttpPost, ActionName("Delete")]
+        //[ValidateAntiForgeryToken]
+        //public ActionResult DeleteConfirmed(int id)
+        //{
+        //    Expert expert = db.Experts.Find(id);
+        //    db.Experts.Remove(expert);
+        //    db.SaveChanges();
+        //    return RedirectToAction("Index");
+        //}
 
         protected override void Dispose(bool disposing)
         {
